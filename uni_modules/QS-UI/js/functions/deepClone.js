516eeec094ac391b67f90bd92e0ce1af484b2e37
胡安宁 <494843897@qq.com>
2021-03-08 19:48:40 +0800

    update

--- uni_modules/QS-UI/js/functions/deepClone.js
+++ uni_modules/QS-UI/js/functions/deepClone.js
@@ -1,14 +1,19 @@
-module.exports = function deepClone(source) {
-  if (!source && typeof source !== 'object') {
-    throw new Error('error arguments', 'deepClone')
-  }
-  const targetObj = source.constructor === Array ? [] : {}
-  Object.keys(source).forEach(keys => {
-    if (source[keys] && typeof source[keys] === 'object') {
-      targetObj[keys] = deepClone(source[keys])
-    } else {
-      targetObj[keys] = source[keys]
-    }
-  })
-  return targetObj
+module.exports = function deepClone(source) {	//摘自网上， 稍作改动
+	if (!source && typeof source !== 'object') {
+		throw new Error('error arguments', 'deepClone')
+	}
+	if(source.constructor === Date) return new Date(source);
+	const targetObj = source.constructor === Array ? [] : {}
+	Object.keys(source).forEach(keys => {
+		if (source[keys] && typeof source[keys] === 'object') {
+			if (source[keys].constructor === Date) {
+				targetObj[keys] = new Date(source[keys]);
+			} else {
+				targetObj[keys] = deepClone(source[keys]);
+			}
+		} else {
+			targetObj[keys] = source[keys]
+		}
+	})
+	return targetObj
 }
